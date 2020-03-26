2d161615e89f10d0d8e5f19bc3ffd2d65be9e201
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-26 19:12:36 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,12 @@
 class Helpers
 
-  def current_user
-    
+  def current_user(hash)
+    @user = User.find_by(id: hash[:user_id])
+    @user
   end 
   
-  def is_logged_in? 
+  def is_logged_in?(hash)
+    if hash[:user_id] == sesssion[:user_id]
     
   end 
 
