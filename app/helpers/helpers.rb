862cd1a7b3cab2d8735856ef7088824af104aa26
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-26 19:30:23 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,10 @@
 class Helpers
 
-  def current_user(hash)
+  def self.current_user(hash)
     @user = User.find(hash[:user_id])
   end 
   
-  def is_logged_in?(hash)
+  def self.is_logged_in?(hash)
     !!hash[:user_id] 
   end 
 
